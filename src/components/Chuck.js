960f337b1cabe212627da50a90e1c03faccea2ce
Reinoptland <reinoptland@gmail.com>
2020-09-01 16:49:40 +0200

    Make button to fetch more jokes

--- src/components/Chuck.js
+++ src/components/Chuck.js
@@ -3,22 +3,26 @@ import axios from "axios";
 
 export default function Chuck() {
   const [joke, setJoke] = useState(null);
+
+  console.log("WHAT IS JOKE:", joke);
+
+  async function getChuckJoke() {
+    const response = await axios.get("https://api.chucknorris.io/jokes/random");
+
+    console.log("RES:", response);
+    setJoke(response.data.value);
+  }
+
   useEffect(() => {
-    console.log("let us fetch a joke!");
-
-    async function getChuckJoke() {
-      const response = await axios.get(
-        "https://api.chucknorris.io/jokes/random"
-      );
-
-      console.log("RES:", response);
-      setJoke(response.data.value);
-    }
-
     getChuckJoke();
   }, []);
 
-  return <h1>{joke}</h1>;
+  return (
+    <div>
+      <h1>{joke}</h1>
+      <button onClick={getChuckJoke}>MORE NORRIS</button>
+    </div>
+  );
 }
 
 // 1. X import useEffect
@@ -30,3 +34,7 @@ export default function Chuck() {
 // 7. X import a useState hook
 // 8. X setJoke with the response data
 // 9. X display the joke
+
+// 1. Create a button
+// 2. add an event listener
+// 3. Make the request again
