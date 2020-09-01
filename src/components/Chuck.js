7a55b79a43bf08fef899e058cbe11d2e7e969520
Reinoptland <reinoptland@gmail.com>
2020-09-01 16:38:02 +0200

    Steps 1, 2 & 3

--- src/components/Chuck.js
+++ src/components/Chuck.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Chuck() {
+  useEffect(() => {
+    console.log("let us fetch a joke!");
+  }, []);
+
   return <h1>Hi .. TODO: Joke here</h1>;
 }
 
-// 1. import useEffect
-// 2. use the useEffect and pass it a callback
-// 3. Test!
+// 1. X import useEffect
+// 2. X use the useEffect and pass it a callback
+// 3. X Test!
 // 4. import axios
 // 5. make a request to the chuck norris api (async function)
 // 6. check the response
